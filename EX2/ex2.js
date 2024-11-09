29c58d7212a33c734a1fe1779bcbd860275e26d9
bruna126lopes <brunasilvalopes629@gmail.com>
2024-11-09 17:13:00 -0300

    Add files via upload

---
+++ EX2/ex2.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("Digite um número:"));
+
+if (numero > 0) {
+  alert("O número é positivo.");
+} else if (numero < 0) {
+  alert("O número é negativo.");
+} else {
+  alert("O número é zero.");
+}
